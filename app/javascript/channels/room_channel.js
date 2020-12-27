cfe4555abeb807b73e191cae54a5c54a28d9cbcc
OhtakeYusuke <o.yusuke0501@gmail.com>
2020-12-28 01:20:07 +0900

    websocket停止

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,42 +1,42 @@
 import consumer from "./consumer"
 
-const appRoom = consumer.subscriptions.create("RoomChannel", {
-  connected() {
+// const appRoom = consumer.subscriptions.create("RoomChannel", {
+//   connected() {
     
-  },
+//   },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+//   disconnected() {
+//     // Called when the subscription has been terminated by the server
+//   },
 
-  received(data) {
-    const messages = document.getElementById("messages");
-    messages.insertAdjacentHTML("afterbegin", data["message"]);
-  },
+//   received(data) {
+//     const messages = document.getElementById("messages");
+//     messages.insertAdjacentHTML("afterbegin", data["message"]);
+//   },
 
-  speak: function(message) {
-    // console.log("check")
-    return this.perform('speak', {message: message});
-  }
-});
+//   speak: function(message) {
+//     // console.log("check")
+//     return this.perform('speak', {message: message});
+//   }
+// });
 
-window.addEventListener("keypress", function(e) {
-  if (e.keyCode === 13) {
-    appRoom.speak(e.target.value);
-    e.target.value = '';
-    e.preventDefault();
-  }
-})
+// window.addEventListener("keypress", function(e) {
+//   if (e.keyCode === 13) {
+//     appRoom.speak(e.target.value);
+//     e.target.value = '';
+//     e.preventDefault();
+//   }
+// })
 
-document.addEventListener("turbolinks:load", function(){
-  const submit = document.getElementById("submit");
-  const chat = document.getElementById("chat-field");
-  submit.addEventListener("click", function(e){
-    appRoom.speak(chat.value);
-    chat.value = "";
-    e.preventDefault();
-  })
-})
+// document.addEventListener("turbolinks:load", function(){
+//   const submit = document.getElementById("submit");
+//   const chat = document.getElementById("chat-field");
+//   submit.addEventListener("click", function(e){
+//     appRoom.speak(chat.value);
+//     chat.value = "";
+//     e.preventDefault();
+//   })
+// })
 
 document.addEventListener("turbolinks:load", function(){
   const text = document.getElementById("chat-field");
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,11 +1,12 @@
-class MessageBroadcastJob < ApplicationJob
-  queue_as :default
+# class MessageBroadcastJob < ApplicationJob
+#   queue_as :default
 
-  def perform(message)
-    ActionCable.server.broadcast 'room_channel', message: render_message(message)
-  end
+#   def perform(message)
+#     ActionCable.server.broadcast 'room_channel', message: render_message(message)
+#   end
 
-  def render_message(message)
-    ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
-  end
-end
+#   def render_message(message)
+#     ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
+#   end
+# end
+
